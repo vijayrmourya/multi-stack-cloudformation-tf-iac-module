95eca2e7359952819ad79c09ea25e1e0e79c0289
Vijay Mourya <vijayrmourya@gmail.com>
2026-01-04 21:51:54 +0530

    Add variables for CloudFormation module

---
+++ cloudformation-module/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "stack_name" {
+  description = "CloudFormation stack name"
+  type        = string
+  default     = "lab-vpc-stack"
+}
+
+variable "cf_parameters" {
+  description = "Map of CloudFormation template parameters (if any)"
+  type        = map(string)
+  default     = {}
+}
+
+variable "ubuntu_ami" {
+  description = "Optional explicit Ubuntu AMI ID to use. If empty, Terraform will look up the latest Ubuntu 22.04 AMI via data.aws_ami.ubuntu.id"
+  type        = string
+  default     = ""
+}
